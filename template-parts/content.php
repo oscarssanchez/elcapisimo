0c48d2aca0a050d117cfae1bd30b511dd25b153f
Oscar Sanchez <osanpk@comunidad.unam.mx>
2018-05-25 14:59:29 -0400

    Initial commit for elcapisimo theme

---
+++ template-parts/content.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package elcapisimo
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( get_elcapisimo_elements_class_slug() ); ?>>
+	<header class="entry-header">
+		<?php
+		if ( 'post' === get_post_type() ) :
+		?>
+		<div class="entry-meta">
+			<?php
+			elcapisimo_posted_on();
+			the_category();
+			?>
+		</div><!-- .entry-meta -->
+		<?php endif;
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php elcapisimo_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+		the_excerpt( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'elcapisimo' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'elcapisimo' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
